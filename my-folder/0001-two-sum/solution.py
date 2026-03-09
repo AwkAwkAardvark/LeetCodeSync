b3dac749369542eeb38a3a4edc15ace6579ff604
AwkAwkAardvark <a081127@naver.com>
2026-03-09 12:13:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.6 MB (19.01%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,19 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hmap = {}
+        hmap={}
         for idx, val in enumerate(nums):
-            # print(f"idx: {idx}")
-            # print(f"val: {val}")
-            complement = target-val
-            # print(f"complement: {complement}")
-            # print(f'hmap so far... : {hmap}')
-            if complement in hmap:
-                # print("Done!")
-                # print(f"idx: {idx}")
-                # print(f"val: {val}")
-                # print(f"hmap[complement]: {hmap[complement]}")
-                return [idx, hmap[complement]]
+            if val in hmap:
+                return [idx,hmap[val]]
             else:
-                hmap[val]=idx
-                # print(f"NOT done! Added {hmap[val]}")
-                # print(f"{hmap[val]}: {idx}")
+                hmap[target-val]=idx
